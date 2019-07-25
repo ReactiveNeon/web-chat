ea98862e84c9c6631bfde6b74a369b980b22129b
Ubuntu <ubuntu@ip-172-31-4-51.us-east-2.compute.internal>
2019-07-25 01:45:53 +0000

    changed port to 3000 from 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var port = 80;
+var port = 3000;
 
 app.use(express.static('./client'));
 
